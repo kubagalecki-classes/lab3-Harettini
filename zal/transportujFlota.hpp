7e88d778006e98df7caec5fbb7b0c8e2d9c0cde4
Harettini <trol199717@o2.pl>
2020-11-14 12:15:45 +0000

    wow

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -7,9 +7,9 @@ unsigned int transportujFlota(unsigned int towar)
 unsigned int zagle=0, wyslane=0, pom;
 Stocznia stocznia{};
 Statek* s;
- while (wyslane<towar)
+ while (wyslane<=towar)
  {s = stocznia();
- pom=s.transportuj();
+ pom=s->transportuj();
  wyslane=wyslane+pom;
  if(dynamic_cast<Zaglowiec*>(s))zagle++;
  delete s;
