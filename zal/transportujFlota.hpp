b44d74e6f9d095a5e84c0be9c71a5bed1e259b79
Harettini <trol199717@o2.pl>
2020-11-14 12:41:40 +0000

    abc

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,15 +4,15 @@
 
 unsigned int transportujFlota(unsigned int towar)
 { if(towar==0)    return 0;
-unsigned int zagle=0, wyslane=0, pom;
+unsigned int z=0, w=0, pom;
 Stocznia stocznia{};
 Statek* s;
  while (1)
  {s = stocznia();
-   pom=s->transportuj();
-   wyslane=wyslane+pom;
-    if((bool)dynamic_cast<Zaglowiec*>(s))zagle++;
+   pom = s->transportuj();
+   w = w + pom;
+    if((bool)dynamic_cast<Zaglowiec*>(s))z = z + 1;
     delete s;
-    if(wyslane>=towar)    return zagle;
+    if(w >= towar)    return z;
  }
 }
