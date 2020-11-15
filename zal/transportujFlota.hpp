e02ea034862bc6f637a54f171d2e1ba03e39d6f8
Harettini <trol199717@o2.pl>
2020-11-15 09:33:27 +0000

    a teraz?

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -6,14 +6,13 @@ unsigned int transportujFlota(unsigned int towar)
 { if(towar==0)    return 0;
 unsigned int z=0, w=0, pom;
 Stocznia stocznia{};
-Zaglowiec* za = new Zaglowiec;
 Statek* s;
  while (1)
  {s = stocznia();
    pom = s->transportuj();
    w = w + pom;
  
-    if(dynamic_cast < Zaglowiec* > (s)==dynamic_cast < Zaglowiec* > (za)) z++;
+    if(dynamic_cast < Zaglowiec* > (s)) z++;
     if(w >= towar)    return z;
     
     delete s;
